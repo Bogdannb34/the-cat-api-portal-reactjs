f98b9fff92e24f6607820a162bdf568f9e0e7154
Bogdannb34 <bogdan22bogdan@gmail.com>
2022-06-16 18:35:21 +0300

    Updates

---
+++ precache-manifest.b61ea1d0679c3a451497fbc0c88fa254.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3df6127086cd787e8df6bee86afbd33",
+    "url": "/the-cat-api-portal-reactjs/index.html"
+  },
+  {
+    "revision": "a65524ee3e2bbcb36534",
+    "url": "/the-cat-api-portal-reactjs/static/css/2.fe62fa80.chunk.css"
+  },
+  {
+    "revision": "18e6551a51e9c16a0920",
+    "url": "/the-cat-api-portal-reactjs/static/css/main.258be8eb.chunk.css"
+  },
+  {
+    "revision": "a65524ee3e2bbcb36534",
+    "url": "/the-cat-api-portal-reactjs/static/js/2.67b8a9e9.chunk.js"
+  },
+  {
+    "revision": "774152ec3dfb5dadc6880f2e7f79e400",
+    "url": "/the-cat-api-portal-reactjs/static/js/2.67b8a9e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18e6551a51e9c16a0920",
+    "url": "/the-cat-api-portal-reactjs/static/js/main.b440f307.chunk.js"
+  },
+  {
+    "revision": "05351ada6f031fb24790",
+    "url": "/the-cat-api-portal-reactjs/static/js/runtime-main.be1632f3.js"
+  },
+  {
+    "revision": "98c1f5ca39d470555dc78e6fc8afdebb",
+    "url": "/the-cat-api-portal-reactjs/static/media/catsDogsLogo.98c1f5ca.png"
+  }
+]);
